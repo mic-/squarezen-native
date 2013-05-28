5ed8dc4bfa7aab164e62d6227a124fdcf6fb8e6e
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-05-28 11:10:23 +0000

    Began breaking the chip emulation code's dependency to Tizen, in order to simplify porting.

--- inc/YmPlayer.h
+++ inc/YmPlayer.h
@@ -23,10 +23,6 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include <FBase.h>
-#include <FIo.h>
-#include <FApp.h>
-#include <FMedia.h>
 #include "YM2149.h"
 #include "Blip_Buffer.h"
 #include "MusicPlayer.h"
@@ -36,20 +32,11 @@ class YmPlayer : public MusicPlayer {
 public:
 	YmPlayer();
 
-	virtual result Prepare(Tizen::Base::String fileName);
-	virtual result Run(uint32_t numSample, int16_t *buffer);
-	virtual result Reset();
-
-	/*result Init(Tizen::Base::String fileName);
-	void Close();
-	void Run(uint32_t numSamples, int16_t *buffer);*/
-
-	const char *GetSongName() const { return (const char*)mSongName; }
-	const char *GetAuthorName() const { return (const char*)mAuthorName; }
-	const char *GetSongComment() const { return (const char*)mSongComment; }
+	virtual int Prepare(std::wstring fileName);
+	virtual int Run(uint32_t numSample, int16_t *buffer);
+	virtual int Reset();
 
 	YmChip mChip;
-
 private:
 	void PresentBuffer(int16_t *out, Blip_Buffer *in);
 
@@ -61,11 +48,6 @@ private:
 	uint8_t mYmRegs[16];
 
 	int16_t *mTempBuffer;
-
-	char *mSongName, *mAuthorName, *mSongComment;
-
-	//Blip_Buffer mBlipBuf;
-	//Blip_Synth<blip_low_quality,82> mSynth[3];
 };
 
 
