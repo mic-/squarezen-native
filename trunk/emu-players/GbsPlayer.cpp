13bd549e3a38137291e88e88f73f76fe3023db4f
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-06-02 19:19:36 +0000

    Various GBS playback fixes.

--- trunk/emu-players/GbsPlayer.cpp
+++ trunk/emu-players/GbsPlayer.cpp
@@ -81,6 +81,7 @@ int GbsPlayer::Prepare(std::wstring fileName)
     AppLog("ID: %c%c%c", mFileHeader.ID[0], mFileHeader.ID[1], mFileHeader.ID[2]);
     AppLog("Load: %#x\nInit: %#x\nPlay: %#x",
     		mFileHeader.loadAddress, mFileHeader.initAddress, mFileHeader.playAddress);
+    AppLog("Timer control %#x, timer modulo %#x", mFileHeader.timerCtrl, mFileHeader.timerMod);
 #endif
 
     numBanks = ((fileSize + mFileHeader.loadAddress - 0x70) + 0x3fff) >> 14;
@@ -154,7 +155,7 @@ int GbsPlayer::Prepare(std::wstring fileName)
 	cart[0xF3] = 0x76;	// HALT
 	cpu.regs.PC = 0xF0;
 	cpu.regs.SP = mFileHeader.SP;
-	cpu.regs.A = 1; // song number
+	cpu.regs.A = 0; // song number
 	cpu.cycles = 0;
 	cpu_execute(mFrameCycles);
 
