434387cd735750bb88a71102883bd5f2cd581b5d
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-06-03 05:59:34 +0000

    Handled some more 6502 opcodes

--- emu-players/Emu6502.cpp
+++ emu-players/Emu6502.cpp
@@ -21,6 +21,11 @@
 				       mRegs.F |= (val == 0) ? Emu6502::FLAG_Z : 0; \
 				       mRegs.F |= (val & 0x80)
 
+#define UPDATE_NZC(val, val2) mRegs.F &= ~(Emu6502::FLAG_Z | Emu6502::FLAG_N) | Emu6502::FLAG_C; \
+				       mRegs.F |= (val == 0) ? Emu6502::FLAG_Z : 0; \
+				       mRegs.F |= (val & 0x80); \
+				       mRegs.F |= (val >= val2) ? FLAG_C : 0
+
 #define ABS_ADDR() (mMemory->ReadByte(mRegs.PC) + ((uint16_t)mMemory->ReadByte(mRegs.PC+1) << 8))
 
 #define ZP_ADDR() (mMemory->ReadByte(mRegs.PC++))
@@ -41,6 +46,7 @@ void Emu6502::Reset()
 void Emu6502::Run(uint32_t maxCycles)
 {
 	uint16_t addr;
+	uint8_t operand;
 
 	while (mCycles < maxCycles) {
 		uint8_t opcode = mMemory->ReadByte(mRegs.PC++);
@@ -173,10 +179,54 @@ void Emu6502::Run(uint32_t maxCycles)
 			ILLEGAL_OP();
 			break;
 
+		case 0x80:
+			ILLEGAL_OP();
+			break;
+
+		case 0x82: case 0x83:
+			ILLEGAL_OP();
+			break;
+
 		case 0x88:		// DEY
 			mRegs.Y--;
 			UPDATE_NZ(mRegs.Y);
 			mCycles += 2;
+			break;
+
+		case 0x89:
+			ILLEGAL_OP();
+			break;
+
+		case 0x8B:
+			ILLEGAL_OP();
+			break;
+
+		case 0x8F:
+			ILLEGAL_OP();
+			break;
+
+		case 0x92: case 0x93:
+			ILLEGAL_OP();
+			break;
+
+		case 0x97:
+			ILLEGAL_OP();
+			break;
+
+		case 0x9B: case 0x9C:
+			ILLEGAL_OP();
+			break;
+
+		case 0x9E: case 0x9F:
+			ILLEGAL_OP();
+			break;
+
+		case 0xA3:
+			ILLEGAL_OP();
+			break;
+
+		case 0xA7:
+			ILLEGAL_OP();
 			break;
 
 		case 0xA9:		// LDA imm
@@ -185,9 +235,67 @@ void Emu6502::Run(uint32_t maxCycles)
 			mCycles += 2;
 			break;
 
+		case 0xAB:
+			ILLEGAL_OP();
+			break;
+
+		case 0xAF:
+			ILLEGAL_OP();
+			break;
+
+		case 0xB2: case 0xB3:
+			ILLEGAL_OP();
+			break;
+
+		case 0xB7:
+			ILLEGAL_OP();
+			break;
+
 		case 0xB8:		// CLV
 			mRegs.F &= ~Emu6502::FLAG_V;
 			mCycles += 2;
+			break;
+
+		case 0xBB:
+			ILLEGAL_OP();
+			break;
+
+		case 0xBF:
+			ILLEGAL_OP();
+			break;
+
+		case 0xC0:		// CPY imm
+			operand = mMemory->ReadByte(mRegs.PC++);
+			UPDATE_NZC(mRegs.Y, operand);
+			mCycles += 2;
+			break;
+
+		case 0xC2: case 0xC3:
+			ILLEGAL_OP();
+			break;
+
+		case 0xC4:		// CPY zp
+			operand = mMemory->ReadByte(ZP_ADDR());
+			UPDATE_NZC(mRegs.Y, operand);
+			mCycles += 3;
+			break;
+
+		case 0xC5:		// CMP zp
+			operand = mMemory->ReadByte(ZP_ADDR());
+			UPDATE_NZC(mRegs.A, operand);
+			mCycles += 3;
+			break;
+
+		case 0xC6:		// DEC zp
+			addr = ZP_ADDR();
+			operand = mMemory->ReadByte(addr) - 1;
+			UPDATE_NZ(operand);
+			mMemory->WriteByte(addr, operand);
+			mCycles += 5;
+			break;
+
+		case 0xC7:
+			ILLEGAL_OP();
 			break;
 
 		case 0xC8:		// INY
@@ -196,8 +304,82 @@ void Emu6502::Run(uint32_t maxCycles)
 			mCycles += 2;
 			break;
 
+		case 0xC9:		// CMP imm
+			operand = mMemory->ReadByte(mRegs.PC++);
+			UPDATE_NZC(mRegs.A, operand);
+			mCycles += 2;
+			break;
+
+		case 0xCA:		// DEX
+			mRegs.X--;
+			UPDATE_NZ(mRegs.X);
+			mCycles += 2;
+			break;
+
+		case 0xCB:
+			ILLEGAL_OP();
+			break;
+
+		case 0xCC:		// CPY abs
+			operand = mMemory->ReadByte(ABS_ADDR());
+			mRegs.PC += 2;
+			UPDATE_NZC(mRegs.Y, operand);
+			mCycles += 4;
+			break;
+
+		case 0xCE:		// DEC abs
+			addr = ABS_ADDR();
+			mRegs.PC += 2;
+			operand = mMemory->ReadByte(addr) - 1;
+			UPDATE_NZ(operand);
+			mMemory->WriteByte(addr, operand);
+			mCycles += 6;
+			break;
+
+		case 0xCF:
+			ILLEGAL_OP();
+			break;
+
 		case 0xD2: case 0xD3: case 0xD4:
 			ILLEGAL_OP();
+			break;
+
+		case 0xD5:		// CMP zp,X
+			operand = mMemory->ReadByte(ZPX_ADDR());
+			UPDATE_NZC(mRegs.A, operand);
+			mCycles += 4;
+			break;
+
+		case 0xD6:		// DEC zp,X
+			addr = ZPX_ADDR();
+			operand = mMemory->ReadByte(addr) - 1;
+			UPDATE_NZ(operand);
+			mMemory->WriteByte(addr, operand);
+			mCycles += 6;
+			break;
+
+		case 0xD7:
+			ILLEGAL_OP();
+			break;
+
+		case 0xDA: case 0xDB: case 0xDC:
+			ILLEGAL_OP();
+			break;
+
+		case 0xDF:
+			ILLEGAL_OP();
+			break;
+
+		case 0xE0:		// CPX imm
+			operand = mMemory->ReadByte(mRegs.PC++);
+			UPDATE_NZC(mRegs.X, operand);
+			mCycles += 2;
+			break;
+
+		case 0xE4:		// CPX zp
+			operand = mMemory->ReadByte(ZP_ADDR());
+			UPDATE_NZC(mRegs.X, operand);
+			mCycles += 3;
 			break;
 
 		case 0xE8:		// INX
@@ -208,6 +390,13 @@ void Emu6502::Run(uint32_t maxCycles)
 
 		case 0xEA:		// NOP
 			mCycles += 2;
+			break;
+
+		case 0xEC:		// CPX abs
+			operand = mMemory->ReadByte(ABS_ADDR());
+			mRegs.PC += 2;
+			UPDATE_NZC(mRegs.X, operand);
+			mCycles += 4;
 			break;
 
 		case 0xF2: case 0xF3: case 0xF4:
