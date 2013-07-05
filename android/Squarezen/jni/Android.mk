8b956ceaa7f5ddacc63cd5c13182cbabe78dd3a3
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-07-05 12:23:40 +0000

    Updated the Android.mk file with references to the LHA source code

--- android/Squarezen/jni/Android.mk
+++ android/Squarezen/jni/Android.mk
@@ -11,6 +11,8 @@ LOCAL_ARM_MODE := arm
 LOCAL_CFLAGS += -ffast-math -O3
 FILE_LIST := $(wildcard $(LOCAL_PATH)/../../../emu-players/*.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+LHA_FILE_LIST := $(wildcard $(LOCAL_PATH)/../../../tizen/serviceapp/lha/*.c)
+LOCAL_SRC_FILES += $(LHA_FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES += emu-players.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../emu-players
 LOCAL_LDLIBS += -llog -lz
