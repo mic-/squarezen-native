50bc3d990b45684e2ac50d9b0c88f78568345d3b
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-08-27 15:09:46 +0000

    Began implementing a few Z80 instructions

--- emu-players/Z80.h
+++ emu-players/Z80.h
@@ -31,6 +31,17 @@ public:
 	void Run(uint32_t maxCycles);
 	void SetMapper(MemoryMapper *mapper) { mMemory = mapper; }
 
+	enum {
+		FLAG_C = 0x01,
+		FLAG_N = 0x02,
+		FLAG_P = 0x04,
+		FLAG_Y = 0x08,	// bit 3 of the result byte
+		FLAG_H = 0x10,
+		FLAG_X = 0x20,	// bit 5 of the result byte
+		FLAG_Z = 0x40,
+		FLAG_S = 0x80,
+	};
+
 	struct  __attribute__ ((__packed__)) {
 		uint8_t F,A, C,B, E,D, L,H;
 		uint16_t SP, PC;
