f2d1fbed066a5e1a3c7a1a8777a539e948db1066
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-08-29 12:17:49 +0000

    Added a few more Z80 opcodes

--- trunk/emu-players/Z80.cpp
+++ trunk/emu-players/Z80.cpp
@@ -45,7 +45,36 @@ static uint8_t gParityTable[256];
 	R1 = J; \
 	mCycles += 8
 
-#define CMD_GROUP_1OP(operation, base, R1) \
+#define CMD_GROUP_1OP(operation, base) \
+	operation(mRegs.B); \
+	break; \
+	case base+1: \
+	operation(mRegs.C); \
+	break; \
+	case base+2: \
+	operation(mRegs.D); \
+	break; \
+	case base+3: \
+	operation(mRegs.E); \
+	break; \
+	case base+4: \
+	operation(mRegs.H); \
+	break; \
+	case base+5: \
+	operation(mRegs.L); \
+	break; \
+	case base+6: \
+	addr = ((uint16_t)mRegs.H << 8) + mRegs.L; \
+	operand = mMemory->ReadByte(addr); \
+	operation(operand); \
+	mMemory->WriteByte(addr, operand); \
+	mCycles += 7; \
+	break; \
+	case base+7: \
+	operation(mRegs.A); \
+	break
+
+#define CMD_GROUP_2OP(operation, base, R1) \
 	operation(R1, mRegs.B); \
 	break; \
 	case base+1: \
@@ -167,6 +196,42 @@ static uint8_t gParityTable[256];
 
 // ====
 
+#define RL8(R) \
+	temp8 = (R & 0x80) ? Z80::FLAG_C : 0;\
+    R = (R << 1) | (mRegs.F & Z80::FLAG_C); \
+	mRegs.F = R & (Z80::FLAG_S | Z80::FLAG_X | Z80::FLAG_Y); \
+	mRegs.F |= gParityTable[R]; \
+	mRegs.F |= temp8; \
+    mRegs.F |= (R == 0) ? Z80::FLAG_Z : 0; \
+    mCycles += 8
+
+#define RR8(R) \
+	temp8 = (R & 0x01) ? Z80::FLAG_C : 0; \
+    R = (R >> 1) | ((mRegs.F & Z80::FLAG_C) << 7); \
+	mRegs.F = R & (Z80::FLAG_S | Z80::FLAG_X | Z80::FLAG_Y); \
+	mRegs.F |= gParityTable[R]; \
+	mRegs.F |= temp8; \
+    mRegs.F |= (R == 0) ? Z80::FLAG_Z : 0; \
+    mCycles += 8
+
+#define SRA8(R) \
+	mRegs.F = (R & 0x01) ? Z80::FLAG_C : 0; \
+    R = (R >> 1) | (R & 0x80); \
+	mRegs.F |= R & (Z80::FLAG_S | Z80::FLAG_X | Z80::FLAG_Y); \
+	mRegs.F |= gParityTable[R]; \
+    mRegs.F |= (R == 0) ? Z80::FLAG_Z : 0; \
+    mCycles += 8
+
+#define SRL8(R) \
+	mRegs.F = (R & 0x01) ? Z80::FLAG_C : 0; \
+    R >>= 1; \
+	mRegs.F |= R & (Z80::FLAG_S | Z80::FLAG_X | Z80::FLAG_Y); \
+	mRegs.F |= gParityTable[R]; \
+    mRegs.F |= (R == 0) ? Z80::FLAG_Z : 0; \
+    mCycles += 8
+
+// ====
+
 #define ILLEGAL_OP() NLOGE("Z80", "Run(): Illegal opcode: %#x at PC=%#x", opcode, mRegs.PC); \
 					 mCycles += 2; \
 					 mRegs.PC++
@@ -238,48 +303,69 @@ void Z80::Run(uint32_t maxCycles)
 			break;
 
 		case 0x40:	// LD B,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x40, mRegs.B);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x40, mRegs.B);
 			break;
 		case 0x48:	// LD C,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x48, mRegs.C);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x48, mRegs.C);
 			break;
 		case 0x50:	// LD D,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x50, mRegs.D);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x50, mRegs.D);
 			break;
 		case 0x58:	// LD E,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x58, mRegs.E);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x58, mRegs.E);
 			break;
 		case 0x60:	// LD H,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x60, mRegs.H);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x60, mRegs.H);
 			break;
 		case 0x68:	// LD L,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x68, mRegs.L);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x68, mRegs.L);
 			break;
 		case 0x78:	// LD A,R2
-			CMD_GROUP_1OP(MOVE_REG8_REG8, 0x78, mRegs.A);
+			CMD_GROUP_2OP(MOVE_REG8_REG8, 0x78, mRegs.A);
 			break;
 
 		case 0x80:	// ADD A,R2
-			CMD_GROUP_1OP(ADD8, 0x80, mRegs.A);
+			CMD_GROUP_2OP(ADD8, 0x80, mRegs.A);
 			break;
 		case 0x88:	// ADC A,R2
-			CMD_GROUP_1OP(ADC8, 0x88, mRegs.A);
+			CMD_GROUP_2OP(ADC8, 0x88, mRegs.A);
 			break;
 		case 0x90:	// SUB A,R2
-			CMD_GROUP_1OP(SUB8, 0x90, mRegs.A);
+			CMD_GROUP_2OP(SUB8, 0x90, mRegs.A);
 			break;
 
 		case 0xA0:	// AND A,R2
-			CMD_GROUP_1OP(AND8, 0xA0, mRegs.A);
+			CMD_GROUP_2OP(AND8, 0xA0, mRegs.A);
 			break;
 		case 0xA8:	// XOR A,R2
-			CMD_GROUP_1OP(XOR8, 0xA8, mRegs.A);
+			CMD_GROUP_2OP(XOR8, 0xA8, mRegs.A);
 			break;
 		case 0xB0:	// OR A,R2
-			CMD_GROUP_1OP(OR8, 0xB0, mRegs.A);
+			CMD_GROUP_2OP(OR8, 0xB0, mRegs.A);
 			break;
 		case 0xB8:	// CP A,R2
-			CMD_GROUP_1OP(CP8, 0xB8, mRegs.A);
+			CMD_GROUP_2OP(CP8, 0xB8, mRegs.A);
+			break;
+
+		case 0xCB:	// bit instruction prefix
+			opcode2 = mMemory->ReadByte(mRegs.PC++);
+			switch (opcode2) {
+			case 0x10:	// RL R
+				CMD_GROUP_1OP(RL8, 0x10);
+				break;
+			case 0x18:	// RR R
+				CMD_GROUP_1OP(RR8, 0x18);
+				break;
+			case 0x28:	// SRA R
+				CMD_GROUP_1OP(SRA8, 0x28);
+				break;
+			case 0x38:	// SRL R
+				CMD_GROUP_1OP(SRL8, 0x38);
+				break;
+			default:
+				ILLEGAL_OP2();
+				break;
+			}
 			break;
 
 		case 0xDD:	// IX prefix
@@ -313,6 +399,14 @@ void Z80::Run(uint32_t maxCycles)
 				ADC8(mRegs.A, mRegs.ixl);
 				mCycles += 4;
 				break;
+			case 0x94:	// SUB A,IXH
+				SUB8(mRegs.A, mRegs.ixh);
+				mCycles += 4;
+				break;
+			case 0x95:	// SUB A,IXL
+				SUB8(mRegs.A, mRegs.ixl);
+				mCycles += 4;
+				break;
 			case 0xA4:	// AND A,IXH
 				AND8(mRegs.A, mRegs.ixh);
 				mCycles += 4;
@@ -335,6 +429,14 @@ void Z80::Run(uint32_t maxCycles)
 				break;
 			case 0xB5:	// OR A,IXL
 				OR8(mRegs.A, mRegs.ixl);
+				mCycles += 4;
+				break;
+			case 0xBC:	// CP A,IXH
+				CP8(mRegs.A, mRegs.ixh);
+				mCycles += 4;
+				break;
+			case 0xBD:	// CP A,IXL
+				CP8(mRegs.A, mRegs.ixl);
 				mCycles += 4;
 				break;
 			default:
@@ -374,6 +476,14 @@ void Z80::Run(uint32_t maxCycles)
 				ADC8(mRegs.A, mRegs.iyl);
 				mCycles += 4;
 				break;
+			case 0x94:	// SUB A,IYH
+				SUB8(mRegs.A, mRegs.iyh);
+				mCycles += 4;
+				break;
+			case 0x95:	// SUB A,IYL
+				SUB8(mRegs.A, mRegs.iyl);
+				mCycles += 4;
+				break;
 			case 0xA4:	// AND A,IYH
 				AND8(mRegs.A, mRegs.iyh);
 				mCycles += 4;
@@ -396,6 +506,14 @@ void Z80::Run(uint32_t maxCycles)
 				break;
 			case 0xB5:	// OR A,IYL
 				OR8(mRegs.A, mRegs.iyl);
+				mCycles += 4;
+				break;
+			case 0xBC:	// CP A,IYH
+				CP8(mRegs.A, mRegs.iyh);
+				mCycles += 4;
+				break;
+			case 0xBD:	// CP A,IYL
+				CP8(mRegs.A, mRegs.iyl);
 				mCycles += 4;
 				break;
 			default:
