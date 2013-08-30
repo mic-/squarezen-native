5105856771125e83938ef0d1167b218a1dc9c979
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-08-30 13:14:18 +0000

    Added a few more HuC6280 opcodes

---
+++ emu-players/HesPlayer.h
@@ -0,0 +1,60 @@
+/*
+ * HesPlayer.h
+ *
+ *  Created on: Aug 30, 2013
+ *
+ * Copyright 2013 Mic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HESPLAYER_H_
+#define HESPLAYER_H_
+
+#include <string>
+#include <stdint.h>
+#include "MusicPlayer.h"
+#include "HuC6280.h"
+#include "HesMapper.h"
+
+
+class HesPlayer : public MusicPlayer
+{
+public:
+	HesPlayer();
+	virtual ~HesPlayer();
+
+	virtual int Prepare(std::string fileName);
+	virtual int Run(uint32_t numSamples, int16_t *buffer);
+	virtual int Reset();
+
+	typedef struct __attribute__ ((__packed__))
+	{
+		char ID[4];
+		uint8_t version;
+		uint8_t firstSong;
+		uint16_t initAddress;
+		uint8_t MPR[8];
+		char subID[4];
+		uint32_t dataSize;
+		uint32_t loadAddress;
+		uint32_t reserved;
+	} HesFileHeader;
+
+private:
+	HesFileHeader mFileHeader;
+	HuC6280 *m6280;
+	HesMapper *mMemory;
+};
+
+#endif
--- emu-players/HuC6280.cpp
+++ emu-players/HuC6280.cpp
@@ -21,6 +21,19 @@
 #include "NativeLogger.h"
 #include "HuC6280.h"
 
+#define UPDATE_NZ(val) mRegs.F &= ~(HuC6280::FLAG_Z | HuC6280::FLAG_N); \
+				       mRegs.F |= ((uint8_t)val == 0) ? HuC6280::FLAG_Z : 0; \
+				       mRegs.F |= (val & 0x80)
+
+// ====
+
+#define COND_BRANCH(cc) \
+	if ((cc)) { relAddr = mMemory->ReadByte(mRegs.PC++); \
+	addr = mRegs.PC + relAddr; \
+    mCycles += 2; \
+    mRegs.PC = addr; } else { mRegs.PC++; } \
+    mCycles += 2
+
 // ====
 
 #define ILLEGAL_OP() NLOGE("HuC6280", "Run(): Illegal opcode: %#x at PC=%#x", opcode, mRegs.PC); \
@@ -54,10 +67,15 @@ void HuC6280::Run(uint32_t maxCycles)
 			mRegs.F &= ~FLAG_T;
 			mCycles += 3;
 			break;
+
+		case 0x10:	// BPL rel
+			COND_BRANCH((mRegs.F & HuC6280::FLAG_N) == 0);
+			break;
 		case 0x18:	// CLC
 			mRegs.F &= ~(FLAG_C | FLAG_T);
 			mCycles += 2;
 			break;
+
 		case 0x22:	// SAX
 			temp8 = mRegs.A;
 			mRegs.A = mRegs.X;
@@ -65,11 +83,16 @@ void HuC6280::Run(uint32_t maxCycles)
 			mRegs.F &= ~FLAG_T;
 			mCycles += 3;
 			break;
+
+		case 0x30:	// BMI rel
+			COND_BRANCH(mRegs.F & HuC6280::FLAG_N);
+			break;
 		case 0x38:	// SEC
 			mRegs.F &= ~FLAG_T;
 			mRegs.F |= FLAG_C;
 			mCycles += 2;
 			break;
+
 		case 0x42:	// SAY
 			temp8 = mRegs.A;
 			mRegs.A = mRegs.Y;
@@ -77,37 +100,91 @@ void HuC6280::Run(uint32_t maxCycles)
 			mRegs.F &= ~FLAG_T;
 			mCycles += 3;
 			break;
+
+		case 0x50:	// BVC rel
+			COND_BRANCH((mRegs.F & HuC6280::FLAG_V) == 0);
+			break;
+		case 0x54:	// CSL
+			// ToDo: tell player that the CPU speed has changed
+			if (mSpeed) {
+				maxCycles >>= 1;
+			}
+			mSpeed = 0;
+			mCycles += 3;
+			break;
 		case 0x58:	// CLI
 			mRegs.F &= ~(FLAG_I | FLAG_T);
 			mCycles += 2;
 			break;
+
 		case 0x62:	// CLA
 			mRegs.A = 0;
 			mRegs.F &= ~FLAG_T;
 			mCycles += 2;
+			break;
+
+		case 0x70:	// BVS rel
+			COND_BRANCH(mRegs.F & HuC6280::FLAG_V);
 			break;
 		case 0x78:	// SEI
 			mRegs.F &= ~FLAG_T;
 			mRegs.F |= FLAG_I;
 			mCycles += 2;
 			break;
+
 		case 0x82:	// CLX
 			mRegs.X = 0;
 			mRegs.F &= ~FLAG_T;
 			mCycles += 2;
 			break;
+
+		case 0x90:	// BCC rel
+			COND_BRANCH((mRegs.F & HuC6280::FLAG_C) == 0);
+			break;
+
+		case 0xA8:	// TAY
+			mRegs.Y = mRegs.A;
+			UPDATE_NZ(mRegs.Y);
+			mCycles += 2;
+			break;
+		case 0xAA:	// TAX
+			mRegs.X = mRegs.A;
+			UPDATE_NZ(mRegs.X);
+			mCycles += 2;
+			break;
+
+		case 0xB0:	// BCS rel
+			COND_BRANCH(mRegs.F & HuC6280::FLAG_C);
+			break;
 		case 0xB8:	// CLV
 			mRegs.F &= ~(FLAG_V | FLAG_T);
 			mCycles += 2;
 			break;
+
 		case 0xC2:	// CLY
 			mRegs.Y = 0;
 			mRegs.F &= ~FLAG_T;
 			mCycles += 2;
 			break;
+
+		case 0xD0:	// BNE rel
+			COND_BRANCH((mRegs.F & HuC6280::FLAG_Z) == 0);
+			break;
+		case 0xD4:	// CSH
+			// ToDo: tell player that the CPU speed has changed
+			if (!mSpeed) {
+				maxCycles <<= 1;
+			}
+			mSpeed = 1;
+			mCycles += 3;
+			break;
 		case 0xD8:	// CLD
 			mRegs.F &= ~(FLAG_D | FLAG_T);
 			mCycles += 2;
+			break;
+
+		case 0xF0:	// BEQ rel
+			COND_BRANCH(mRegs.F & HuC6280::FLAG_Z);
 			break;
 		case 0xF4:	// SET
 			mRegs.F |= FLAG_T;
@@ -118,6 +195,7 @@ void HuC6280::Run(uint32_t maxCycles)
 			mRegs.F |= FLAG_D;
 			mCycles += 2;
 			break;
+
 		default:
 			ILLEGAL_OP();
 			break;
