9baee057a6c0ae09739f31793e6f4c13e8c1371c
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-09-01 15:04:21 +0000

    Added a few more 6280 opcodes

---
+++ emu-players/KonamiVrc6.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2013 Mic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define NLOG_LEVEL_DEBUG 0
+
+#include "NativeLogger.h"
+#include "KonamiVrc6.h"
+
+
+void KonamiVrc6Channel::Reset()
+{
+	// ToDo: implement
+}
+
+
+void KonamiVrc6Channel::Step()
+{
+	// ToDo: implement
+
+}
+
+
+void KonamiVrc6Channel::Write(uint32_t addr, uint8_t data)
+{
+	// ToDo: implement
+}
+
+
+void KonamiVrc6::Reset()
+{
+	// ToDo: implement
+}
+
+
+void KonamiVrc6::Step()
+{
+	// ToDo: implement
+}
+
+
+void KonamiVrc6::Write(uint32_t addr, uint8_t data)
+{
+	// ToDo: implement
+}
---
+++ emu-players/KonamiVrc6.h
@@ -0,0 +1,58 @@
+/*
+ * KonamiVrc6.h
+ *
+ *  Created on: Sep 1, 2013
+ *
+ * Copyright 2013 Mic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KONAMIVRC6_H_
+#define KONAMIVRC6_H_
+
+#include <stdint.h>
+#include "Oscillator.h"
+
+
+class KonamiVrc6Channel : public Oscillator
+{
+public:
+	virtual ~KonamiVrc6Channel() {}
+
+	virtual void Reset();
+	virtual void Step();
+	virtual void Write(uint32_t addr, uint8_t val);
+};
+
+
+class KonamiVrc6
+{
+public:
+	void Reset();
+	void Step();
+	void Write(uint32_t addr, uint8_t data);
+
+	// Channel enumerators
+	enum
+	{
+		CHN_PULSE1  = 0,
+		CHN_PULSE2  = 1,
+		CHN_SAW 	= 2,
+	};
+
+private:
+	KonamiVrc6Channel mChannels[3];
+};
+
+#endif
