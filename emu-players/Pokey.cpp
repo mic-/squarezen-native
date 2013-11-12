a3316163db5e069a8b854558f0710b1bd5d37134
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-11-12 11:57:48 +0000

    Added classes for SAP playback (mostly stubbed right now)

---
+++ emu-players/Pokey.cpp
@@ -0,0 +1,1 @@
+#include "Pokey.h"
---
+++ emu-players/SapMapper.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013 Mic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define NLOG_LEVEL_VERBOSE 0
+#define NLOG_TAG "SapMapper"
+
+#include <string.h>
+#include <stddef.h>
+#include "NativeLogger.h"
+#include "SapMapper.h"
+
+
+SapMapper::SapMapper(uint32_t numRomBanks)
+{
+	// ToDo: implement
+}
+
+
+SapMapper::~SapMapper()
+{
+	// ToDo: implement
+}
+
+
+uint8_t SapMapper::ReadByte(uint32_t addr)
+{
+	// ToDo: implement
+	return 0;
+}
+
+
+void SapMapper::WriteByte(uint32_t addr, uint8_t data)
+{
+	// ToDo: implement
+}
+
+
+void SapMapper::Reset()
+{
+	// ToDo: implement
+}
