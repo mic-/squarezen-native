f760342b355d3c67ca07ee2d88d1a27c46603f39
micol972@gmail.com <micol972@gmail.com@2533e8ba-15a9-86f0-8117-ac2bd44e916f>
2013-11-14 09:43:48 +0000

    Altered the YM effect handling a bit. Digidrums are now somewhat working. Sync-buzz has been implemented but not tested.

--- emu-players/YmPlayer.cpp
+++ emu-players/YmPlayer.cpp
@@ -212,6 +212,14 @@ MusicPlayer::Result YmPlayer::Prepare(std::string fileName)
 
 	mChip.Reset();
 
+	if (mYmData[0x13 & 0x04]) {
+		mChip.mDigiDrumFormat = YmChip::DIGI_DRUM_U4;
+	} else if (mYmData[0x13] & 0x02) {
+		mChip.mDigiDrumFormat = YmChip::DIGI_DRUM_S8;
+	} else {
+		mChip.mDigiDrumFormat = YmChip::DIGI_DRUM_U8;
+	}
+
 	mFrame = 0;
 	mNumFrames = (uint32_t)mYmData[0x0E] << 8;
 	mNumFrames += mYmData[0x0F];
